85e9a797ec4d99f43bb7e207b2018473677706ae
ascendingmine <avyang@s.sfusd.edu>
2025-04-09 11:04:57 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,23 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    int a = 0;
+    for(int i = 0; i < myList.size(); i++){
+      if(myList.get(i).length==len){
+        a++;
+      }
+    }
+    return a;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-
+    for(int i = 0; i <animals.size(); i++){
+      if(animals.get(i).length==len){
+        animals.remove(i);
+        i--;
+      }
+    }
   }
 }
